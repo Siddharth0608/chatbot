a416657af1dd724960cf0197178cc56426b316d6
Siddharth Sharma <113353294+Siddharth0608@users.noreply.github.com>
2024-08-19 15:43:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import torch
-from peft import PeftModel, PeftConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import requests
 import re
 # Load the model and tokenizer in a Streamlit cached function
