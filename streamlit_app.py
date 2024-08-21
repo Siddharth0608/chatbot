906cad0b9d9b8a66afd93c403a2b0aebef030395
Siddharth Sharma <113353294+Siddharth0608@users.noreply.github.com>
2024-08-21 22:39:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ prompt = prompt + " " + f"<genre>{selected_option}</genre>"
 
 if st.button("Generate Story"):
     if prompt:
-        api_url = "https://787e-35-247-191-92.ngrok-free.app/generate/"  # Replace with your ngrok URL
+        api_url = "https://b283-35-237-16-35.ngrok-free.app/generate/"  # Replace with your ngrok URL
         response = requests.post(api_url, json={"prompt": prompt})
         match = re.search(r"(?:assistant\s+.*?){2}(.*)", response.json()["generated_text"], re.DOTALL)
         assistant_output = match.group(1).strip()
